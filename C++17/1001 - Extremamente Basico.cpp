167630fcaecf49d01eefdee4ff6dcc0079014395
Gabriel Henrique da Silva <gabrielhenri0012@gmail.com>
2021-03-18 11:10:46 -0300

    C++17 \ 1001 - Extremamente Basico

---
+++ C++17/1001 - Extremamente Basico.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 	
+ 	//Criando/Declarando variaveis inteiras
+    int a, b, x;
+	
+	//Realizando a entrada atravez do terminal/CMD ("Entrada do Usuario")
+	cin >> a;
+	cin >> b;
+	
+	//Calculando e atribuindo o resultado da soma (a + b) em x
+	x = a + b;
+	
+	//Monstrando resultado da Soma que foi atribuida a X
+	cout << "X = " << x << endl;
+	
+    return 0;
+}
